ae2dbc0c58184faf3b4c03643e0beacef313922d
aShchepelin <aleksandr.shchepelin@ru.nestle.com>
2022-10-24 19:17:52 +0300

    create develop branch

---
+++ MoviesStoreApp/MoviesStoreApp/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  MoviesStoreApp
+//
+//  Created by Александр Андреевич Щепелин on 24.10.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(
+        _: UIApplication,
+        didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
